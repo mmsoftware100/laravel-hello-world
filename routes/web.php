be0a2aa855d4eff827134ccbbb15cd6f448090d8
Cisco Ramon <aungkoman@gmail.com>
2025-05-07 10:39:00 +0630

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,11 @@ Route::get('/', function () {
 Route::get('/greeting', function () {
     return "<h2>Good Morning</h2>";
 });
+
+Route::get('/bye', function () {
+    return "<h2>See you</h2>";
+});
+
+Route::get('/fun-fact', function () {
+    return "<h2>Good Morning</h2>";
+});
