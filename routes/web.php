64806d6b77ba99f50c932438dfa4fdf8debc2550
Cisco Ramon <aungkoman@gmail.com>
2025-05-07 14:40:07 +0630

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,17 @@ Route::get('/', function () {
     // return "<h2>Hello, We're at get / route </h2>";
     // $html = <<<HTML
     //     <!DOCTYPE html>
-    //     <html lang="en">
-    //     <head>
-    //         <meta charset="UTF-8">
-    //         <meta name="viewport" content="width=device-width, initial-scale=1">
-    //         <title>My First HTML Page</title>
-    //     </head>
-    //     <body>
-    //         <h1>Hello, World!</h1>
-    //         <p>This is a basic HTML file returned from a Laravel route.</p>
-    //     </body>
-    //     </html>
+        // <html lang="en">
+        // <head>
+        //     <meta charset="UTF-8">
+        //     <meta name="viewport" content="width=device-width, initial-scale=1">
+        //     <title>My First HTML Page</title>
+        // </head>
+        // <body>
+        //     <h1>Hello, World!</h1>
+        //     <p>This is a basic HTML file returned from a Laravel route.</p>
+        // </body>
+        // </html>
     //     HTML;
     // return $html;
     return view('hello');
