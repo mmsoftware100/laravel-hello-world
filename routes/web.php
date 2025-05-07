e46065df147629845c9e31e69089cf1d2661b9c8
Cisco Ramon <aungkoman@gmail.com>
2025-05-07 10:13:24 +0630

    routing basic added

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return "<h2> Hello World</h2>";
+    return "<h2>Hello, We're at get / route </h2>";
+});
+
+Route::get('/greeting', function () {
+    return "<h2>Good Morning</h2>";
 });
